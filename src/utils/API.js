bc9d181e08734b300ff105576e49ae9a08678c86
Kayvon Kazemini <kayvonk@gmail.com>
2021-05-29 19:16:50 -0600

    Changed getPost

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,5 +3,5 @@ import axios from "axios";
 export const API = {
   savePost: (post) => axios.post("https://voyagrapp.herokuapp.com/api/posts", post),
 
-  getPost: (sub) => axios.get("https://voyagrapp.herokuapp.com/api/dashboard/" + sub)
+  getPost: (sub) => axios.get("https://voyagr-app.netlify.app/dashboard" + sub)
 }
