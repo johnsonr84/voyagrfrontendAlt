507dbbf15c88310fcc682c4acf071cdac2e71839
Kayvon Kazemini <kayvonk@gmail.com>
2021-06-03 21:10:01 -0600

    Added modal input

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const API = {
+  saveUser: (user) => axios.post("api/users", user),
+
   savePost: (post) => axios.post("api/posts", post),
 
   getPost: (sub) => axios.get("api/dashboard/" + sub)
