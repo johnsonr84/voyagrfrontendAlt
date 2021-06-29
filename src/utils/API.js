30e41b643c957f2db29e27fb32a2594d78adf1a4
Kayvon Kazemini <kayvonk@gmail.com>
2021-06-28 19:57:03 -0600

    Added API route

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 export const API = {
-  saveUser: (user) => axios.post("api/users", user),
+  saveUser: (user) => axios.post("https://voyagrapp.herokuapp.com/api/users", user),
 
-  getUser: (uid) => axios.get("api/users/", uid),
+  getUser: (uid) => axios.get("https://voyagrapp.herokuapp.com/api/users/", uid),
 
-  getUserByParam: (uid) => axios.get("api/users/" + uid),
+  getUserByParam: (uid) => axios.get("https://voyagrapp.herokuapp.com/api/users/" + uid),
 
-  updateUser: (uid) => axios.put("api/users/", uid),
+  updateUser: (uid) => axios.put("https://voyagrapp.herokuapp.com/api/users/", uid),
 
-  savePost: (post) => axios.post("api/posts", post),
+  savePost: (post) => axios.post("https://voyagrapp.herokuapp.com/api/posts", post),
 
-  getPost: (uid) => axios.get("api/posts/" + uid)
+  getPost: (uid) => axios.get("https://voyagrapp.herokuapp.com/api/posts/" + uid)
 }
